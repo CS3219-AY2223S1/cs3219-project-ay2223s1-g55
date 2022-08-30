ec2bc164fac5703fb0b5379a7d18d9a2ed0673f8
glennljw <glennljw7@gmail.com>
2022-08-30 15:59:51 +0800

    Add route to dashboard page and dashboard component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,31 @@
-import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import SignupPage from './components/SignupPage';
-import {Box} from "@mui/material";
+import { Box } from '@mui/material';
+import Dashboard from './components/Dashboard';
 
 function App() {
-    return (
-        <div className="App">
-            <Box display={"flex"} flexDirection={"column"} padding={"4rem"}>
-                <Router>
-                    <Routes>
-                        <Route exact path="/" element={<Navigate replace to="/signup" />}></Route>
-                        <Route path="/signup" element={<SignupPage/>}/>
-                    </Routes>
-                </Router>
-            </Box>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Box display={'flex'} flexDirection={'column'} padding={'4rem'}>
+        <Router>
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={<Navigate replace to="/signup" />}
+            ></Route>
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </Router>
+      </Box>
+    </div>
+  );
 }
 
 export default App;
