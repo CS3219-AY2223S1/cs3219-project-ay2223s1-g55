a9660f1f92db5b3b7f3b22747893564b51ec537b
Zhang Zhikai <zhangzhikai71@gmail.com>
2022-09-10 17:03:36 +0800

    initial set up of matching service in index js

--- matching-service/index.js
+++ matching-service/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
+// Used to create socket.io
 import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +14,34 @@ app.get('/', (req, res) => {
   res.send('Hello World from matching-service');
 });
 
-const httpServer = createServer(app);
+const PORT = process.env.PORT || 8001;
 
-httpServer.listen(8001);
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+// Run when clinet connects
+io.on('connection', (socket) => {
+  console.log('New WS Connection...');
+  // OPEN BIDIRECTIONAL COMMUNICATION
+  // emit to the single client that is connecting
+  socket.emit('message', 'Welcome to Matching Service');
+
+  // Broadcast when a user connects
+  // inform everyone(all other clients) except user thats connecting
+  socket.broadcast.emit('message', 'A user has joined the chat');
+
+  // emit to all the clients
+  // io.emit()
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat');
+  });
+});
+
+httpServer.listen(PORT, () => console.log(`matching-service listening on port ${PORT}`));
+
+// TODO: Create a socket.io server instance
+// TODO: Log something to console upon socket connection for a sense
+// TODO: Run matching-service and test connection by using Postman
+// httpServer.listen(8001);
