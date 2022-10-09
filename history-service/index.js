20ed2ec31c0416e1608b762e2fc2d0446ac7a4db
takufunkai <takufunkai@gmail.com>
2022-10-09 22:22:42 +0800

    Remove comments

--- history-service/index.js
+++ history-service/index.js
@@ -13,14 +13,6 @@ import {
 
 const router = express.Router();
 
-/**
- * Use cases
- * 1. User wants to see their own history
- * 2. User wants to see their completion page (lsit of completed questions)
- * 3. User wants to see the details for a specific record
- * 4. A record is logged after a user starts a session
- */
-
 // Controller will contain all the History-defined Routes
 router.get('/', (_, res) => res.send('History-service is up and running!'));
 router.get('/user/:userId/records/', listUserRecords);
@@ -31,4 +23,4 @@ app.use('/api/history', router).all((_, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
 });
 
-app.listen(process.env.PORT || 8002, () => console.log(`user-service listening on port ${process.env.PORT || 8002}`));
+app.listen(process.env.PORT || 8002, () => console.log(`history-service listening on port ${process.env.PORT || 8002}`));
