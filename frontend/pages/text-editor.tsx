930318a7d805a02fa7d9975b6f8bac43e8f57731
Asher <77272176+asherhy@users.noreply.github.com>
2022-10-10 05:21:05 +0800

    Integrate Quill into NextJs

---
+++ frontend/pages/text-editor.tsx
@@ -0,0 +1,25 @@
+import dynamic from 'next/dynamic';
+import 'react-quill/dist/quill.snow.css';
+
+const QuillNoSSRWrapper = dynamic(import('react-quill'), {
+  ssr: false,
+  loading: () => <p>Loading ...</p>,
+});
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    ['bold', 'italic', 'underline'],
+    [{ color: [] }, { background: [] }],
+    [{ script: 'sub' }, { script: 'super' }],
+    [{ align: [] }],
+    ['image', 'blockquote', 'code-block'],
+    ['clean'],
+  ],
+};
+function TextEditor() {
+  return <QuillNoSSRWrapper theme="snow" modules={modules} />;
+}
+
+export default TextEditor;
