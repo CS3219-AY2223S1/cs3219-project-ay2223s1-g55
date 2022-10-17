557c04780822b37b2a62ca9135b51c9bd046b1dd
Asher <77272176+asherhy@users.noreply.github.com>
2022-10-18 02:33:06 +0800

    Add dbname

--- editor-service/model/repository.js
+++ editor-service/model/repository.js
@@ -6,7 +6,11 @@ import mongoose from 'mongoose';
 
 const mongoDB = process.env.ENV == 'PROD' ? process.env.DB_CLOUD_URI : process.env.DB_LOCAL_URI;
 
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mongoDB, {
+  dbname: 'editor-service',
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
