1cf1dff31e0192a2061ebd80dc4a3ed3ea156836
Asher <77272176+asherhy@users.noreply.github.com>
2022-11-04 07:04:11 +0800

    Update repository.js

--- editor-service/model/repository.js
+++ editor-service/model/repository.js
@@ -5,7 +5,7 @@ import 'dotenv/config.js';
 import mongoose from 'mongoose';
 
 const mongoDB = process.env.ENV == 'PROD' ? process.env.DB_CLOUD_URI : process.env.DB_LOCAL_URI;
-const dbName = process.env.ENV === 'testDB' ? 'test' : 'editorDB';
+const dbName = process.env.ENV === 'test' ? 'testEditorDB' : 'editorDB';
 
 mongoose.connect(mongoDB, {
   dbname: dbName,
