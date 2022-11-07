e1a6ef833280932469bbde4ceae4908cd570a8c7
takufunkai <takufunkai@gmail.com>
2022-11-07 19:58:42 +0800

    Remove layer of description

--- history-service/test/test.js
+++ history-service/test/test.js
@@ -27,69 +27,67 @@ const clearDatabaseAfterHook = () => {
 }
 
 describe('History-Service Basic Test', async () => {
-  describe('Lmao', () => {
-    /**
-     * Set-up
-     */
-    // Generate stubs, setup before hook to populate db, and return the stubs.
-    const stubs = populateDatabaseBeforeHook(20);
-    // Clear the test db after the test suite.
-    clearDatabaseAfterHook();
+  /**
+   * Set-up
+   */
+  // Generate stubs, setup before hook to populate db, and return the stubs.
+  const stubs = populateDatabaseBeforeHook(20);
+  // Clear the test db after the test suite.
+  clearDatabaseAfterHook();
 
-    /*
-     * Test the /GET route
-     */
-    it('Tests the endpoint', async () => { await getIndex() });
+  /*
+   * Test the /GET route
+   */
+  it('Tests the endpoint', async () => { await getIndex() });
 
-    /* 
-     * Test the GET /records/:username route
-     * Passes if correct number of records per user is returned
-     */
-    it('list user records', async () => {
-      const tests = allDummyUsernames.map(async username => {
-        const expectedRecordsCount = findUserRecordsFromStubs(stubs, username).length;
+  /* 
+   * Test the GET /records/:username route
+   * Passes if correct number of records per user is returned
+   */
+  it('list user records', async () => {
+    const tests = allDummyUsernames.map(async username => {
+      const expectedRecordsCount = findUserRecordsFromStubs(stubs, username).length;
 
-        const records = await getUserRecords(username);
-        const actualRecordsCount = records.body.length;
+      const records = await getUserRecords(username);
+      const actualRecordsCount = records.body.length;
 
-        assert.equal(expectedRecordsCount, actualRecordsCount);
-      })
-      await Promise.all(tests);
+      assert.equal(expectedRecordsCount, actualRecordsCount);
     })
+    await Promise.all(tests);
+  })
 
-    /* 
-     * Test the GET /completed/:username route
-     * Passes if the completed questions are equal to the inserted ones.
-     */
-    it('list user completed questions', async () => {
-      const tests = allDummyUsernames.map(async username => {
-        const expectedCompletedQuestions = findUserRecordsFromStubs(stubs, username)
-          .map(({ questionName }) => questionName) // Get all questions names
+  /* 
+   * Test the GET /completed/:username route
+   * Passes if the completed questions are equal to the inserted ones.
+   */
+  it('list user completed questions', async () => {
+    const tests = allDummyUsernames.map(async username => {
+      const expectedCompletedQuestions = findUserRecordsFromStubs(stubs, username)
+        .map(({ questionName }) => questionName) // Get all questions names
 
-        const { body: actualCompletedQuestions } = await getUserCompletedQuestions(username)
+      const { body: actualCompletedQuestions } = await getUserCompletedQuestions(username)
 
-        expect(expectedCompletedQuestions).to.include.members(actualCompletedQuestions);
-      })
-      await Promise.all(tests)
+      expect(expectedCompletedQuestions).to.include.members(actualCompletedQuestions);
     })
+    await Promise.all(tests)
+  })
 
-    /* 
-     * Test the GET /experience/:username route
-     * Passes if the experience returned is the same as the calculated inserted experience
-     */
-    it('list user experience', async () => {
-      const tests = allDummyUsernames.map(async username => {
-        const expectedUserExperiencePoints = findUserRecordsFromStubs(stubs, username)
-          .map(({ questionDifficulty }) => questionDifficulty)
-          .reduce((sum, difficulty) => sum + EXPERIENCE_POINTS[`${difficulty}`], 0)
-        const expectedUserExperienceLevel = calculateUserExperienceLevel(expectedUserExperiencePoints);
+  /* 
+   * Test the GET /experience/:username route
+   * Passes if the experience returned is the same as the calculated inserted experience
+   */
+  it('list user experience', async () => {
+    const tests = allDummyUsernames.map(async username => {
+      const expectedUserExperiencePoints = findUserRecordsFromStubs(stubs, username)
+        .map(({ questionDifficulty }) => questionDifficulty)
+        .reduce((sum, difficulty) => sum + EXPERIENCE_POINTS[`${difficulty}`], 0)
+      const expectedUserExperienceLevel = calculateUserExperienceLevel(expectedUserExperiencePoints);
 
-        const { body: { experiencePoints, experienceLevel } } = await getUserExperience(username)
+      const { body: { experiencePoints, experienceLevel } } = await getUserExperience(username)
 
-        expect(experiencePoints).to.eq(expectedUserExperiencePoints);
-        expect(experienceLevel).to.be.eq(expectedUserExperienceLevel)
-      })
-      await Promise.all(tests)
+      expect(experiencePoints).to.eq(expectedUserExperiencePoints);
+      expect(experienceLevel).to.be.eq(expectedUserExperienceLevel)
     })
+    await Promise.all(tests)
   })
 });
