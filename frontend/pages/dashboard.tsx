f58d64d46e4ce71ee93fd0eea61c1eff77a4bb2e
glennljw <glennljw7@gmail.com>
2022-11-09 03:01:22 +0800

    Fix build failure 3

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -46,15 +46,17 @@ export default Dashboard;
 export async function getStaticProps() {
   const { data } = await axios.get(`${URL_QUESTION_QUESTIONS}`);
   const { questions } = data;
-  const titleAndDifficulty = questions?.map((qn) => {
-    const { title, difficulty } = qn;
-    return { title, difficulty };
-  });
   if (!questions) {
     return {
       props: { questions: [] },
     };
   }
+
+  const titleAndDifficulty = questions?.map((qn) => {
+    const { title, difficulty } = qn;
+    return { title, difficulty };
+  });
+
   return {
     props: {
       questions: titleAndDifficulty,
